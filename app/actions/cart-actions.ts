c7e16caa2f77341da7f9663dc383f27ba50c47bf
Ruby Boyarski <rubyboy@gmail.com>
2015-11-30 16:03:55 +0200

    Extract action creators

--- app/actions/cart-actions.ts
+++ app/actions/cart-actions.ts
@@ -1,6 +1,9 @@
+export const ADD_TO_CART:string = 'ADD_TO_CART';
+export const REMOVE_FROM_CART:string = 'REMOVE_FROM_CART';
+
 export function addToCart(id) {
-    return {type:"ADD_TO_CART",id};
+    return {type:ADD_TO_CART,id};
 }
 export function removeFromCart(id) {
-    return {type:"REMOVE_FROM_CART",id};
+    return {type:REMOVE_FROM_CART,id};
 }
--- app/actions/part-actions.ts
+++ app/actions/part-actions.ts
@@ -1,4 +1,6 @@
+export const ADD_PART:string = 'ADD_PART';
+
 var id = 0;
 export function addPart(name) {
-    return {type: "ADD_PART", id: id++, name};
-};
+    return {type: ADD_PART, id: id++, name};
+}
