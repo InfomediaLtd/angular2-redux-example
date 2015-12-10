2c13b060bc182529ecb1e941a26c006a3fe1e48f
Ruby Boyarski <rubyboy@gmail.com>
2015-12-10 10:29:30 +0200

    use constants

--- app/reducers/parts-reducer.ts
+++ app/reducers/parts-reducer.ts
@@ -1,8 +1,9 @@
+import * as PartActions from '../actions/part-actions';
 import partReducer from "./part-reducer"
 
 export default (state = [], action:any = {}) => {
     switch (action.type) {
-        case "ADD_PART":
+        case PartActions.ADD_PART:
             return [...state, partReducer(null, action)];
         default:
             return state;
