b1d3c39060af13390480ef6b5dd42fe196cd0342
Ruby Boyarski <rboyarski@infomedia.com.au>
2016-01-27 13:29:56 +0200

    Part 1

--- app/app.ts
+++ app/app.ts
@@ -1,24 +1,11 @@
 import {Component} from 'angular2/core'
 import {AppStore} from "angular2-redux";
-import {AdminComponent} from "./components/admin-component";
 import {ShoppingComponent} from "./components/shopping-component";
-import {FilmsComponent} from "./components/films-component";
 
 @Component({
     selector: 'my-app',
-    template: `
-        <div class="row">
-            <div class="col-md-6">
-                <admin></admin>
-            </div>
-            <div class="col-md-6">
-                <shopping></shopping>
-                <hr/>
-                <films-component></films-component>
-            </div>
-        </div>
-    `,
-    directives: [ShoppingComponent, AdminComponent, FilmsComponent]
+    template: `<shopping></shopping>`,
+    directives: [ShoppingComponent]
 })
 export class AppComponent {
 
